b52cf2d3c18f0ad37452ba06339f863145bf53d8
Mona Lisa Octocat <bernkra@gmail.com>
2023-09-11 18:24:22 +0200

    Update Final

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import './App.css';
-import logo from './logo.svg';
+import randomColor from 'randomcolor';
+import { useState } from 'react';
 
 export default function App() {
+  const [colorHex, setColorHex] = useState(randomColor());
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>React Random Color Generator</h1>
+
+      <div style={{ backgroundColor: colorHex }}>
+        Generated Color: {colorHex}
+      </div>
+      <br />
+      <button
+        onClick={() => {
+          const newHex = randomColor();
+          setColorHex(newHex);
+        }}
+      >
+        Generate
+      </button>
+    </>
   );
 }
